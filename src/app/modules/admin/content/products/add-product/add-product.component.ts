613a48a0e8408e0af5231d39e66820fd8d6db6f9
Anubhav Saxena <tuthubpro@gmail.com>
2018-04-15 02:51:48 +0530

    add product detail page ready

--- src/app/modules/admin/content/products/add-product/add-product.component.ts
+++ src/app/modules/admin/content/products/add-product/add-product.component.ts
@@ -1,75 +1,111 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ChangeDetectionStrategy, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+import {IOption} from 'ng-select';
+import {Subscription} from 'rxjs/Subscription';
+import {Observable} from 'rxjs/Observable';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {CustomValidators} from 'ng2-validation';
+import {SelectOptionService} from '../../../../../modules/admin/shared/element/select-option.service';
+
+import {Http, Response} from '@angular/http';
+import {FileUploader} from 'ng2-file-upload';
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: [
+    './add-product.component.css',
+    '../../../../../../../node_modules/famfamfam-flags/dist/sprite/famfamfam-flags.min.css'
+  ],
+  encapsulation: ViewEncapsulation.None
 })
-export class AddProductComponent  implements OnInit {
+export class AddProductComponent implements OnInit, OnDestroy {
   myForm: FormGroup;
-  mynumberForm: FormGroup;
-  mytooltipForm: FormGroup;
-  checkdropForm: FormGroup;
-  submitted: boolean;
+  public editor;
+  public editorContent;
+  public editorConfig = {
+    placeholder: 'About Your Self'
+  };
+  uploader: FileUploader = new FileUploader({
+    url: URL,
+    isHTML5: true
+  });
+  simpleOption: Array<IOption> = this.selectOptionService.getCharacters();
+  selectedOption = '3';
 
-  constructor() {
+  characters: Array<IOption>;
 
-    const name = new FormControl('', Validators.required);
-    const password = new FormControl('', Validators.required);
-    const gender = new FormControl('', Validators.required);
-    const email = new FormControl('', [Validators.required, Validators.email]);
+  selectedCharacter = '3';
+  timeLeft = 5;
 
-    const rpassword = new FormControl('', [Validators.required, CustomValidators.equalTo(password)]);
+  countries: Array<IOption> = this.selectOptionService.getCountries();
+  selectedCountry = 'IN';
+  selectedCountries: Array<string> = ['IN', 'BE', 'LU', 'NL'];
+
+  private dataSub: Subscription = null;
+
+  autocompleteItems = ['Alabama', 'Wyoming', 'Henry Die', 'John Doe'];
+  constructor(public selectOptionService: SelectOptionService, public http: Http) {
+
+    const productName = new FormControl('', Validators.required);
+    const productPrice = new FormControl('', Validators.required);
+    const categoryId = new FormControl('', Validators.required);
+    const productImage = new FormControl('', Validators.required);
+    const productDescription = new FormControl('', Validators.required);
+
     this.myForm = new FormGroup({
-      name: name,
-      email: email,
-      password: password,
-      rpassword: rpassword,
-      gender: gender
+      productName: productName,
+      productPrice: productPrice,
+      categoryId: categoryId,
+      productDescription: productDescription,
+      productImage: productImage
     });
     /*Basic validation end*/
-
-    /*number Validation start*/
-    const integer = new FormControl('', [Validators.required, CustomValidators.digits]);
-    const numeric = new FormControl('', [Validators.required, CustomValidators.number]);
-    const greater = new FormControl('', [Validators.required, CustomValidators.gt(50)]);
-    const less = new FormControl('', [Validators.required, CustomValidators.lt(50)]);
-
-    this.mynumberForm = new FormGroup({
-      integer: integer,
-      numeric: numeric,
-      greater: greater,
-      less: less
-    });
-    /*number validation end*/
-
-    /*Tooltip Validation Start*/
-    const usernameP = new FormControl('', [Validators.required]);
-    const EmailP = new FormControl('', [Validators.required, Validators.email]);
-    this.mytooltipForm = new FormGroup({
-      usernameP: usernameP,
-      EmailP: EmailP,
-    });
-    /*Tooltip Validation End*/
-
-    /* component form */
-    const area = new FormControl('', [Validators.required]);
-    const job = new FormControl('', [Validators.required]);
-    this.checkdropForm = new FormGroup({
-      area: area,
-      job: job,
-    });
-    /* end component form */
   }
-
   ngOnInit() {
+    this.runTimer();
+    this.dataSub = this.selectOptionService.loadCharacters().subscribe((options) => {
+      this.characters = options;
+    });
+
+    setTimeout(() => {
+      this.editorContent = this.editorContent;
+      console.log('you can use the quill instance object to do something', this.editor);
+      // this.editor.disable();
+    }, 2800);
   }
 
-  onSubmit() {
-    this.submitted = true;
-    console.log(this.myForm);
+  onEditorBlured(quill) {
+    console.log('editor blur!', quill);
+  }
+
+  onEditorFocused(quill) {
+    console.log('editor focus!', quill);
+  }
+
+  onEditorCreated(quill) {
+    this.editor = quill;
+    /*console.log('quill is ready! this is current quill instance object', quill);*/
+  }
+
+  onContentChanged({ quill, html, text }) {
+    /*console.log('quill content is changed!', quill, html, text);*/
+  }
+
+  ngOnDestroy() {
+    if (this.dataSub !== null) { this.dataSub.unsubscribe(); }
+  }
+
+  runTimer() {
+    const timer = setInterval(() => {
+      this.timeLeft -= 1;
+      if (this.timeLeft === 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
   }
 
 }
