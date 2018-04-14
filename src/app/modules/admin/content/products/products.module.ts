711e08c80dff3c827118700b44435f3b85df0636
Anubhav Saxena <tuthubpro@gmail.com>
2018-04-15 01:28:02 +0530

    product edit page added

--- src/app/modules/admin/content/products/products.module.ts
+++ src/app/modules/admin/content/products/products.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {DataTableModule} from 'angular2-datatable';
 import {HttpModule} from '@angular/http';
+import {SelectModule} from 'ng-select';
+import {SelectOptionService} from '../../../admin/shared/element/select-option.service';
+import {TagInputModule} from 'ngx-chips';
 import {RouterModule, Routes} from '@angular/router';
 import {SharedModule} from '../../shared/shared.module';
 import { AddProductComponent } from './add-product/add-product.component';
@@ -30,10 +33,13 @@ export const ProductsRoutes: Routes = [{
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
+    SelectModule,
+    TagInputModule,
     DataTableModule,
     RouterModule.forChild(ProductsRoutes),
     SharedModule
   ],
+  providers: [SelectOptionService],
   declarations: [ViewProductComponent, EditProductComponent, AddProductComponent]
 })
 export class ProductsModule { }
