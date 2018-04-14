60b12a383fe6ce8be744ea8c747656b5dcdd7a3b
Anubhav Saxena <tuthubpro@gmail.com>
2018-04-15 02:44:40 +0530

    edit page completed phase 2 completed

--- src/app/modules/admin/content/products/edit-product/edit-product.component.ts
+++ src/app/modules/admin/content/products/edit-product/edit-product.component.ts
@@ -24,7 +24,7 @@ const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 })
 export class EditProductComponent implements OnInit, OnDestroy {
   myForm: FormGroup;
-  mynumberForm: FormGroup;
+
   mytooltipForm: FormGroup;
   checkdropForm: FormGroup;
   submitted: boolean;
@@ -32,18 +32,18 @@ export class EditProductComponent implements OnInit, OnDestroy {
   public editor;
   public editorContent;
   public editorConfig = {
-    placeholder: "About Your Self"
+    placeholder: 'About Your Self';
+
   };
   uploader: FileUploader = new FileUploader({
     url: URL,
     isHTML5: true
   });
-  hasBaseDropZoneOver = false;
-  hasAnotherDropZoneOver = false;
   simpleOption: Array<IOption> = this.selectOptionService.getCharacters();
   selectedOption = '3';
-  isDisabled = true;
+
   characters: Array<IOption>;
+
   selectedCharacter = '3';
   timeLeft = 5;
 
@@ -54,43 +54,6 @@ export class EditProductComponent implements OnInit, OnDestroy {
   private dataSub: Subscription = null;
 
   autocompleteItems = ['Alabama', 'Wyoming', 'Henry Die', 'John Doe'];
-  autocompleteItemsAsObjects = [
-    {value: 'Alabama', id: 0},
-    {value: 'Wyoming', id: 1},
-    {value: 'Coming', id: 2},
-    {value: 'Josephin Doe', id: 3},
-    {value: 'Henry Die', id: 4},
-    {value: 'Lary Doe', id: 5},
-    {value: 'Alice', id: 6},
-    {value: 'Alia', id: 7},
-    {value: 'Suzen', id: 8},
-    {value: 'Michael Scofield', id: 9},
-    {value: 'Irina Shayk', id: 10},
-    {value: 'Sara Tancredi', id: 11},
-    {value: 'Daizy Mendize', id: 12},
-    {value: 'Loren Scofield', id: 13},
-    {value: 'Shayk', id: 14},
-    {value: 'Sara', id: 15},
-    {value: 'Doe', id: 16},
-    {value: 'Lary', id: 17},
-    {value: 'Roni Sommerfield', id: 18},
-    {value: 'Mickey Amavisca', id: 19},
-    {value: 'Dorethea Hennigan', id: 20},
-    {value: 'Marisha Haughey', id: 21},
-    {value: 'Justin Czajkowski', id: 22},
-    {value: 'Reyes Hodges', id: 23},
-    {value: 'Vicky Hadley', id: 24},
-    {value: 'Lezlie Baumert', id: 25},
-    {value: 'Otha Vanorden', id: 26},
-    {value: 'Glayds Inabinet', id: 27},
-    {value: 'Hang Owsley', id: 28},
-    {value: 'Carlotta Buttner', id: 29},
-    {value: 'Randa Vanloan', id: 30},
-    {value: 'Elana Fulk', id: 31},
-    {value: 'Amos Spearman', id: 32},
-    {value: 'Samon', id: 33},
-    {value: 'John Doe', id:  34}
-  ];
   constructor(public selectOptionService: SelectOptionService, public http: Http) {
 
     const productName = new FormControl('', Validators.required);
@@ -107,36 +70,6 @@ export class EditProductComponent implements OnInit, OnDestroy {
       productImage: productImage
     });
     /*Basic validation end*/
-
-    /*number Validation start*/
-    const integer = new FormControl('', [Validators.required, CustomValidators.digits]);
-    const numeric = new FormControl('', [Validators.required, CustomValidators.number]);
-    const greater = new FormControl('', [Validators.required, CustomValidators.gt(50)]);
-    const less = new FormControl('', [Validators.required, CustomValidators.lt(50)]);
-
-    this.mynumberForm = new FormGroup({
-      integer: integer,
-      numeric: numeric,
-      greater: greater,
-      less: less
-    });
-    /*number validation end*/
-
-    /*Tooltip Validation Start*/
-    const usernameP = new FormControl('', [Validators.required]);
-    this.mytooltipForm = new FormGroup({
-      usernameP: usernameP
-    });
-    /*Tooltip Validation End*/
-
-    /* component form */
-    const area = new FormControl('', [Validators.required]);
-    const job = new FormControl('', [Validators.required]);
-    this.checkdropForm = new FormGroup({
-      area: area,
-      job: job,
-    });
-    /* end component form */
   }
   ngOnInit() {
     this.runTimer();
@@ -181,17 +114,4 @@ export class EditProductComponent implements OnInit, OnDestroy {
     }, 1000);
   }
 
-  public requestAutocompleteItems = (text: string): Observable<Response> => {
-    const url = `https://api.github.com/search/repositories?q=${text}`;
-    return this.http
-      .get(url)
-      .map(data => data.json().items.map(item => item.full_name));
-  }
-  fileOverBase(e: any): void {
-    this.hasBaseDropZoneOver = e;
-  }
-
-  fileOverAnother(e: any): void {
-    this.hasAnotherDropZoneOver = e;
-  }
 }
