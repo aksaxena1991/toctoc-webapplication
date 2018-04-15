7bec5df0f848c32370f4c6d5c8f256b11b9496be
Anubhav Saxena <tuthubpro@gmail.com>
2018-04-15 12:26:15 +0530

    menu item added

--- src/app/modules/admin/shared/menu-items/menu-items.ts
+++ src/app/modules/admin/shared/menu-items/menu-items.ts
@@ -42,6 +42,13 @@ const MENUITEMS = [
         icon: 'ti-home'
       },
       {
+        state: 'user',
+        short_label: 'U',
+        name: 'User Profile',
+        type: 'link',
+        icon: 'ti-user'
+      },
+      {
         state: 'roles',
         short_label: 'R',
         name: 'Roles',
@@ -137,115 +144,6 @@ const MENUITEMS = [
             name: 'Add Product'
           }
         ]
-      }
-    ]
-  },
-  {
-    label: 'Map And Extra Pages ',
-    main: [
-      {
-        state: 'map',
-        short_label: 'M',
-        name: 'Maps',
-        type: 'link',
-        icon: 'ti-map-alt'
-      },
-      {
-        state: 'auth',
-        short_label: 'A',
-        name: 'Auth',
-        type: 'sub',
-        icon: 'ti-id-badge',
-        children: [
-          {
-            state: 'login',
-            type: 'link',
-            name: 'Login',
-            target: true
-          }, {
-            state: 'registration',
-            type: 'link',
-            name: 'Registration',
-            target: true
-          }
-        ]
-      },
-      {
-        state: 'error',
-        external: 'http://lite.codedthemes.com/guru-able/error.html',
-        name: 'Error',
-        type: 'external',
-        icon: 'ti-layout-list-post',
-        target: true
-      },
-      {
-        state: 'user',
-        short_label: 'U',
-        name: 'User Profile',
-        type: 'link',
-        icon: 'ti-user'
-      }
-    ]
-  },
-  {
-    label: 'Other',
-    main: [
-      {
-        state: '',
-        short_label: 'M',
-        name: 'Menu Levels',
-        type: 'sub',
-        icon: 'ti-direction-alt',
-        children: [
-          {
-            state: '',
-            name: 'Menu Level 2.1',
-            target: true
-          }, {
-            state: '',
-            name: 'Menu Level 2.2',
-            type: 'sub',
-            children: [
-              {
-                state: '',
-                name: 'Menu Level 2.2.1',
-                target: true
-              },
-              {
-                state: '',
-                name: 'Menu Level 2.2.2',
-                target: true
-              }
-            ]
-          }, {
-            state: '',
-            name: 'Menu Level 2.3',
-            target: true
-          }, {
-            state: '',
-            name: 'Menu Level 2.4',
-            type: 'sub',
-            children: [
-              {
-                state: '',
-                name: 'Menu Level 2.4.1',
-                target: true
-              },
-              {
-                state: '',
-                name: 'Menu Level 2.4.2',
-                target: true
-              }
-            ]
-          }
-        ]
-      },
-      {
-        state: 'simple-page',
-        short_label: 'S',
-        name: 'Simple Page',
-        type: 'link',
-        icon: 'ti-layout-sidebar-left'
       }
     ]
   }
