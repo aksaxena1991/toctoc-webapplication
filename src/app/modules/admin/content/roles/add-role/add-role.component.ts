0a0621be212ab6756f1b86d8f0017382eea914ff
Anubhav Saxena <tuthubpro@gmail.com>
2018-04-23 23:45:19 +0530

    Add role done

--- src/app/modules/admin/content/roles/add-role/add-role.component.ts
+++ src/app/modules/admin/content/roles/add-role/add-role.component.ts
@@ -2,8 +2,10 @@ import {Component, ChangeDetectionStrategy, OnInit, ViewEncapsulation} from '@an
 import {Http, RequestOptions, Headers, Response} from '@angular/http';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/map';
+import {url} from '../../../../expoters/url';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import { RolesService } from '../../../../services/roles/roles.services';
+import {RolesService} from '../../../../services/roles/roles.services';
+
 @Component({
   selector: 'app-add-role',
   templateUrl: './add-role.component.html',
@@ -16,27 +18,27 @@ import { RolesService } from '../../../../services/roles/roles.services';
 })
 export class AddRoleComponent implements OnInit {
   myForm: FormGroup;
+  baseUrl = url;
 
   constructor(private _roleService: RolesService, private http: Http) {
-
     const role_name = new FormControl('', Validators.required);
     this.myForm = new FormGroup({
       role_name: role_name
     });
-    /*Basic validation end*/
   }
+
   onSubmit() {
     const headers = new Headers();
-    headers.append( 'Content-Type', 'application/json' );
-    // headers.append('Content-Type', 'application/x-www-form-urlencoded' );
-    const options = new RequestOptions({ headers: headers });
-
-    this.http.post('http://localhost:3000/api/roles/addRole',
+    headers.append('Content-Type', 'application/json');
+    const options = new RequestOptions({headers: headers});
+    this.http.post(this.baseUrl + 'roles/addRole',
       this.myForm['_value'], options).subscribe((data) => {
-      console.log(data);
+      if (data.status === 200) {
+        this.myForm.reset();
+      }
     });
   }
+
   ngOnInit() {
   }
-
 }
